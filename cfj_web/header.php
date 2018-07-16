9b7c0350dfde78523c682831913ef9e6244b4f66
Iizawa Kuniyuki <>
2018-07-16 20:40:48 +0900

    update google tag manager

--- cfj_web/header.php
+++ cfj_web/header.php
@@ -9,7 +9,14 @@
   <meta http-equiv="Cache-Control" content="no-cache">
   <meta name="google-site-verification" content="wXBVf-ygOR0E2KSveBQ05L6ruZGzWMjpQccd_nx3apQ" />
   <title>Code for Japan</title>
-  <script>
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-K3T5GLH');</script>
+<!-- End Google Tag Manager -->
+<script>
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -19,7 +26,10 @@ ga('send', 'pageview');
 </script>
 </head>
 <body>
-
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K3T5GLH"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 <header>
   <div id="header" class="header">
     <div class="header__inner">
